567837285645d7ceb446c8ff466cc5635e3b07ac
mahmoud842 <mahmoud1hesham2003@gmail.com>
2023-12-21 11:00:55 +0200

    added some comments for compiling and running the game

--- main.c
+++ main.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "structures.h"
+#include "inputValidation.h"
+#include "display.h"
+#include "saveAndLoad.h"
+
+// compile command:
+// gcc main.c structures.c inputValidation.c display.c saveAndLoad.c -o dotsAndLines
+// run: ./dotsAndLines
 
 int main(){
-    printf("hello");
+    printf("hello\n");
+    printf("%d", mainMenuInput(4));
     return 0;
 }
