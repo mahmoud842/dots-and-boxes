9ac4a06ec42b0f37544e9ec7c8197778c084e70c
mahmoud842 <mahmoud1hesham2003@gmail.com>
2023-12-23 19:10:38 +0200

    undo redo done

--- main.c
+++ main.c
@@ -17,6 +17,8 @@ void crokyDisplayState(state * s){
         }
         printf("\n");
     }
+    printf("player 1 score: %d\t player 2 score: %d\n", s->p1Score, s->p2Score);
+    printf("player 1 Moves: %d\t player 2 Moves: %d\n", s->p1Moves, s->p2Moves);
 }
 
 int main(){
@@ -31,18 +33,55 @@ int main(){
             char playerTurn = 1; // 1 means player 1's turn, 2 means player 2's turn, 0 means AI turn
             char playerWon = 0; // 0 means no one won, 1 means player 1 won, 2 means player 2 worn (AI is considered player 2)
             char * action;
+            // construct intial state
             state * s = constructState(gameOptions->gridSize);
+
+            // construct undo and redo state
+            s->turn = playerTurn;
+            undoRedo * uR = constructUndoRedo(gameOptions->gridSize, copyState(s));
+
+            // someFlags for error messages
+            char undoFlag = 0;
+            char redoFlag = 0;
+            char elseFlag = 0;
+            char saveGameFlag = 0;
+
             while(!playerWon){
                 crokyDisplayState(s);
+
+                if (elseFlag){
+                    printf("else error\n");
+                    elseFlag = 0;
+                }
+
+                if (saveGameFlag){
+                    printf("save error\n");
+                    saveGameFlag = 1;
+                }
+
+                // error messages:
+                if (undoFlag){
+                    printf("no undo available\n");
+                    undoFlag = 0;
+                }
+                if (redoFlag){
+                    printf("no redo available\n");
+                    redoFlag = 0;
+                }
+
                 // gameMode: 1 for human, 2 for AI easy, 3 for AI hard
                 s->gameMode = (gameOptions->gameMode == 1) ? (gameOptions->gameMode) : (gameOptions->gameMode + gameOptions->AIDifficulty - 1);
                 // here I should as about input first (savegame, undo ,redo ,..)
+                // printf("player %d %d turn\n", playerTurn, s->turn);
                 char inGameMenu = displayInGameMenu();
                 
+
                 // palce a line
                 if (inGameMenu == 1){
                     char invalidActionFlag = 0;
                     char actionFlag = 0;
+
+                    // printf("inside place line condition\n");
 
                     // if playerTurn = 1 or 2 then human plays
                     if (playerTurn){
@@ -59,11 +98,13 @@ int main(){
                             }
                         } while(invalidActionFlag);
 
+                        
+
                     }
 
                     // AI turn
                     else {
-                        //action = AIAction();
+                        // action = AIAction();
                         // actionFlag = applyStateAction(action, playerTurn, s)
                         printf("AI not available now\n");
                     }
@@ -76,6 +117,8 @@ int main(){
                         else {
                             playerTurn = (playerTurn == 1) ? 0 : 1;
                         }
+                        s->turn = playerTurn;
+                        pushStateToRedoUndo(uR, copyState(s));
                     }
                     else if(actionFlag == 3){
                         printf("player %d win\n", playerTurn);
@@ -85,26 +128,50 @@ int main(){
 
                 // undo
                 else if (inGameMenu == 2){
-
+                    state * tmpS = getUndo(uR);
+                    if (tmpS != NULL){
+                        freeState(s);
+                        s = tmpS;
+                        playerTurn = s->turn;
+                    }
+                    else {
+                        undoFlag = 1;
+                    }
                 }
 
                 // redo
                 else if (inGameMenu == 3){
-
+                    state * tmpS = getRedo(uR);
+                    if (tmpS != NULL){
+                        freeState(s);
+                        s = tmpS;
+                        playerTurn = s->turn;
+                    }
+                    else {
+                        redoFlag = 1;
+                    }
                 }
 
                 // save game
                 else if (inGameMenu == 4){
-
+                    saveGameFlag = 1;
                 }
 
                 // go to main menu
                 else {
-
+                    elseFlag = 1;
                 }
 
                 
             }
+            freeUndoRedo(uR);
+            crokyDisplayState(s);
+            freeState(s);
+            printf("here i should take your name as input");
+            
+            // just to stop it for now (will be changed)
+            char ttt;
+            scanf("%c", ttt);
         }
         else if (gameOptions->loadGame){
 
